5316cbb742d21a0273fe7becc3fa3664401d80b7
Akash <aakasgdev24@gmail.com>
2023-05-18 03:36:18 +0530

    Login Controller added

--- server/routes/UserRoutes.js
+++ server/routes/UserRoutes.js
@@ -1,9 +1,11 @@
 import express from "express";
-import { registerUser } from "../controller/UserController.js";
+import { loginUser, registerUser } from "../controller/UserController.js";
 
 const router = express.Router();
 
 router.post("/register", registerUser);
+router.post("/login", loginUser);
+
 
 let userRouter = router;
 export default  userRouter;
