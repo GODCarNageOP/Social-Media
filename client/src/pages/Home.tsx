ef684a7d306b04ce646081471d42b76e66d677f5
GODCarNageOP <yashvharale@gmail.com>
2023-05-20 02:06:47 +0530

    third commit

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,9 +1,17 @@
-
 const Home = () => {
-    return (
-      <div className="home w-[40%]">home
+  return (
+    <div className="home w-[40%]">
+      <h1 className="text-xl font-semibold my-1 p-4">Home</h1>
+      <div className="homeNavbar flex justify-between h-16">
+        <span className="flex-1 flex justify-center items-center hover:bg-gray-200 font-medium">
+          For you
+        </span>
+        <span className="flex-1 flex justify-center items-center hover:bg-gray-200 font-medium">
+          Following
+        </span>
+      </div>
     </div>
-    )
-  }
-  
-  export default Home
+  );
+};
+
+export default Home;
