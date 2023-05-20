73fef5605b5e0b46cb1914a41b903e6190d14073
Akash <aakasgdev24@gmail.com>
2023-05-21 04:33:14 +0530

    underline-added width-full-don

--- client/src/components/Tweets/TweetsCard.tsx
+++ client/src/components/Tweets/TweetsCard.tsx
@@ -1,13 +1,11 @@
 import React from "react";
 import "./TweetCard.css";
 import profilePic from "../../assets/pofilePic.jpeg";
-import ChatBubbleOutlineIcon from "@mui/icons-material/ChatBubbleOutline";
-import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
-import SignalCellularAltIcon from "@mui/icons-material/SignalCellularAlt";
-import UploadIcon from "@mui/icons-material/Upload";
-import MoreVertIcon from "@mui/icons-material/MoreVert";
+
 import { AiOutlineRetweet, AiOutlineHeart, AiFillHeart } from "react-icons/ai";
 import { MdOutlineGraphicEq } from "react-icons/md";
+import SentimentVeryDissatisfiedIcon from "@mui/icons-material/SentimentVeryDissatisfied";
+
 import { GoComment } from "react-icons/go";
 import { BsUpload, BsThreeDots } from "react-icons/bs";
 import MoreHorizOutlinedIcon from "@mui/icons-material/MoreHorizOutlined";
@@ -38,15 +36,20 @@ const TweetCard: React.FC<TweetCardProps> = ({ tweet }) => {
             <div className="left mr-3">
                 <img src={profilePic} alt="" className="w-11 h-11 rounded-full ml-5 mt-2" />
             </div>
-            <div className="center flex flex-col flex-2 mt-2">
-                <div className="top ">
-                    <div className="profile-details-tweets flex gap-2 w-full  items-center">
+            <div className="center flex flex-col flex-2 mt-2 w-full ">
+                <div className="flex gap-2 w-full   justify-between">
+                    <div className="profile-details-tweets flex gap-2 w-full">
                         <span className="font-bold hover:underline cursor-pointer">{tweet?.name}</span>
-                        <span className="text-gray-600 text-sm"> @{tweet?.userName}</span>
-                        <span className="text-gray-600">•</span>
-                        <span className="text-gray-600 text-sm">{tweet?.joined}</span>
-                        <MoreHorizOutlinedIcon className="icon ml-24 text-gray-600 hover:rounded-full hover:text-blue-500 cursor-pointer" />
+                        <span className="text-gray-600"> @{tweet?.userName}</span>
+                        <span>
+                            .
+                        </span>
+                        <span className="text-gray-600">{tweet?.joined}</span>
                     </div>
+                    <span className="cursor-pointer  tweet-icon hover:text-blue-500 text-gray-500">
+
+                        <BsThreeDots />
+                    </span>
                 </div>
                 <div className="middle flex">
                     <span>{tweet?.content}</span>
@@ -100,7 +103,7 @@ const TweetCard: React.FC<TweetCardProps> = ({ tweet }) => {
                                 {tweet?.likes}
                             </span>
                         </div>
-                        
+
                         <span className="cursor-pointer tweet-icon hover:text-blue-500 text-gray-700 ">
 
                             <BsUpload />
@@ -114,5 +117,31 @@ const TweetCard: React.FC<TweetCardProps> = ({ tweet }) => {
         </div>
     );
 };
+
+
+
+
+
+const More = () => {
+
+
+    return (
+        <div className="overlay h-21 w-76 p-2 bg-white  shadow-lg border rounded-xl z-10 flex flex-col top-[465px] ml-[60px]">
+            <div className="overlayContainer gap-5">
+                <div className="top hover:bg-gray-100 font-medium text-base h-10 p-3 cursor-pointer flex gap-2">
+                    <SentimentVeryDissatisfiedIcon />
+                    <h1>Not interested in this</h1>
+                </div>
+                <div className="bottom hover:bg-gray-100 font-medium text-base h-10 p-3 cursor-pointer flex gap-2">
+                    <SentimentVeryDissatisfiedIcon />
+                    <h1>This trend is harmful or spammy</h1>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+
+
 
 export default TweetCard;
