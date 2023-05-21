a662ffe94142dd9fb35760a5f2bf5aa7021cc62a
Akash <aakasgdev24@gmail.com>
2023-05-21 06:56:54 +0530

    custom -tab-created added-in-home-notifications

---
+++ client/src/components/Tabbar.tsx
@@ -0,0 +1,62 @@
+import React from "react";
+import { Link, useLocation } from "react-router-dom";
+
+interface Tab {
+    label: string;
+    path: string;
+}
+
+interface TabBarProps {
+    tabs: Tab[];
+
+}
+
+const TabBar: React.FC<TabBarProps> = ({ tabs }) => {
+    const location = useLocation();
+    const pathname = location.pathname;
+    const urlAfterSlash = pathname.substring(1);
+
+
+    // if (urlAfterSlash==='') {
+    //     urlAfterSlash='/'
+    // }
+    console.log('tab', tabs, urlAfterSlash);
+
+    console.log(urlAfterSlash === tabs[1].path, tabs[1].path)
+
+    return (
+        <div className="tab-bar w-full">
+            <div className="flex h-14 cursor-pointer">
+                {tabs.map((tab) => (
+
+                    <Link
+                        key={tab.path}
+                        to={tab.path}
+                        className={`flex-1 flex flex-col relative ${urlAfterSlash === tab.path ? "active-link" : "inactive-link"
+                            }`}
+                    >
+                        <span
+                            className={`flex-1 font-bold flex justify-center items-center hover:bg-gray-200 ${urlAfterSlash === tab.path ? "activeTabs" : "deactivate-tabs"
+                                }`}
+                        >
+                            {tab.label}
+                        </span>
+
+                        {/* <div className="activelines"></div> */}
+                        {urlAfterSlash === tab.path.substring(1) ? (
+                            <div className="activelines"></div>
+                        ) : (
+                            <div className="deactivatelines"></div>
+                        )}
+
+
+
+
+                    </Link>
+                ))}
+            </div>
+        </div>
+    );
+};
+
+export default TabBar;
